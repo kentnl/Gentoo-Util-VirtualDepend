324a21f6942a80bf60928f615ee8bf9240b9fab8
Kent Fredric <kentfredric@gmail.com>
2014-10-10 22:14:49 +1300

    initial commit

---
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -0,0 +1,18 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package Gentoo::Util::VirtualDepend;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT:
+
+# AUTHORITY
+
+use Moo;
+
+no Moo;
+
+1;
