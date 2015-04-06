c7a2993b4f8f5bc9049cd061249e4ef37df5973f
Kent Fredric <kentfredric@gmail.com>
2015-04-07 09:02:53 +1200

    v0.002002
     [Mappings]
     - Dropping need for specialcasings for Log-Dispatch

--- lib/Gentoo/Util/VirtualDepend.pm
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Gentoo::Util::VirtualDepend;
 
-our $VERSION = '0.002002';
+our $VERSION = '0.002003';
 
 # ABSTRACT: Hard-coded replacements for perl-core/ dependencies and dependencies with odd names in Gentoo
 
