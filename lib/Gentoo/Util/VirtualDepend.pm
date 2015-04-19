7f86dd0c94465446dc81a925a9de00d464188453
Kent Fredric <kentfredric@gmail.com>
2015-04-19 15:43:42 +1200

    Build results of 9fdec24 (on master)

--- lib/Gentoo/Util/VirtualDepend.pm
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Gentoo::Util::VirtualDepend;
 
-our $VERSION = '0.002003';
+our $VERSION = '0.002004';
 
 # ABSTRACT: Hard-coded replacements for perl-core/ dependencies and dependencies with odd names in Gentoo
 
@@ -188,7 +188,7 @@ Gentoo::Util::VirtualDepend - Hard-coded replacements for perl-core/ dependencie
 
 =head1 VERSION
 
-version 0.002003
+version 0.002004
 
 =head1 SYNOPSIS
 
