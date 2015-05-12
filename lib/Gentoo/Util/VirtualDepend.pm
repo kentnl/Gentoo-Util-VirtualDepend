db225fb4757a8507f2758962b7c4657ca728e01f
Kent Fredric <kentfredric@gmail.com>
2015-05-12 22:11:23 +1200

    v0.002005
     [Dependencies::Stats]
     - Dependencies changed since 0.002004, see misc/*.deps* for details
     - develop: (suggests: ↑1)
    
     [Mappings]
     - Drop specialcasing for Class-ReturnValue

--- lib/Gentoo/Util/VirtualDepend.pm
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Gentoo::Util::VirtualDepend;
 
-our $VERSION = '0.002005';
+our $VERSION = '0.002006';
 
 # ABSTRACT: Hard-coded replacements for perl-core/ dependencies and dependencies with odd names in Gentoo
 
