c678aa640de2f6f6b0ded6f762750022d41af4c3
Kent Fredric <kentfredric@gmail.com>
2015-08-04 22:03:38 +1200

    v0.003003
     [Mappings]
     - Remove special casings for Gtk2Fu: Has gone to CPAN nirvana.
     - Actually remove special casing for Inline-Files

--- lib/Gentoo/Util/VirtualDepend.pm
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Gentoo::Util::VirtualDepend;
 
-our $VERSION = '0.003003';
+our $VERSION = '0.003004';
 
 # ABSTRACT: Hard-coded replacements for perl-core/ dependencies and dependencies with odd names in Gentoo
 
