1ae374e623584595266e0dceca48db90f297bd2f
Kent Fredric <kentfredric@gmail.com>
2016-08-04 06:31:25 +1200

    v0.003017
     - Remove special casing for: Regexp::Common
    
     [Dependencies::Stats]
     - Dependencies changed since 0.003016, see misc/*.deps* for details
     - develop: (suggests: ↑1)

--- lib/Gentoo/Util/VirtualDepend.pm
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Gentoo::Util::VirtualDepend;
 
-our $VERSION = '0.003017';
+our $VERSION = '0.003018';
 
 # ABSTRACT: Hard-coded replacements for perl-core/ dependencies and dependencies with odd names in Gentoo
 
