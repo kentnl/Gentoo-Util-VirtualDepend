09c6ead9cf8b3eae4ffa001c4aef59b035350599
Kent Fredric <kentfredric@gmail.com>
2017-03-07 02:08:18 +1300

    v0.003022
     [Bugfix]
     - Update tests for -Ddefault_inc_excludes_dot
    
     [Dependencies::Stats]
     - Dependencies changed since 0.003021, see misc/*.deps* for details
     - develop: ↑1 (suggests: ↑2)
     - test: ↓1

--- lib/Gentoo/Util/VirtualDepend.pm
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Gentoo::Util::VirtualDepend;
 
-our $VERSION = '0.003022';
+our $VERSION = '0.003023';
 
 # ABSTRACT: Hard-coded replacements for perl-core/ dependencies and dependencies with odd names in Gentoo
 
