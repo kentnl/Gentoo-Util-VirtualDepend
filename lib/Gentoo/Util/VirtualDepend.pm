0c00e2e9091db8b288e62c183fd81619460d01cf
Kent Fredric <kentfredric@gmail.com>
2017-03-19 18:22:49 +1300

    Build results of 4f6f1ae (on master)

--- lib/Gentoo/Util/VirtualDepend.pm
+++ lib/Gentoo/Util/VirtualDepend.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Gentoo::Util::VirtualDepend;
 
-our $VERSION = '0.003023';
+our $VERSION = '0.003024';
 
 # ABSTRACT: Hard-coded replacements for perl-core/ dependencies and dependencies with odd names in Gentoo
 
@@ -207,7 +207,7 @@ Gentoo::Util::VirtualDepend - Hard-coded replacements for perl-core/ dependencie
 
 =head1 VERSION
 
-version 0.003023
+version 0.003024
 
 =head1 SYNOPSIS
 
